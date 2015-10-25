2a0bba7a12b28a6039beddc8681158ba3810b1e4
c4toor <c4toor@yahoo.com>
2015-10-25 15:00:37 +0700

    message

---
+++ ProgrammingAssignment2.R
@@ -0,0 +1,43 @@
+makeCacheMatrix <- function(x = matrix()) {
+        m <- NULL
+        
+        set <- function(y) {
+                x <<- y
+                m <<- NULL
+        }
+        
+        get <- function() x
+        
+        setinv <- function(inverse) m <<- inverse
+        getinv <- function() m
+        
+        list(set = set, get = get, setinv = setinv, getinv = getinv)
+}
+
+cacheinv <- function(x, ...) {
+        m <- x$getinv()
+        
+        if(!is.null(m)) {
+                message("getting cached data")
+                return(m)
+        }
+        
+        data <- x$get()
+        
+        m <- solve(data, ...)
+        
+        x$setinv(m)
+        return(m)
+}
+
+timediff <- function(thematrix){
+        ## @mat: an invertible matrix
+        
+        temp = makeCacheMatrix(thematrix)
+        
+        start.time = Sys.time()
+        cacheinv(temp)
+        dur = Sys.time() - start.time
+        print(dur)
+        
+}
